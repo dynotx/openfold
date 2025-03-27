dc77c5da46f9988c59cc33dc18eb155eda5a22c9
Stephen Malina <1093790+an1lam@users.noreply.github.com>
2025-03-27 14:27:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ else:
 
 setup(
     name='openfold',
-    version='2.2.1+dyno',
+    version='2.2.2+dyno',
     description='A PyTorch reimplementation of DeepMind\'s AlphaFold 2',
     author='OpenFold Team',
     author_email='jennifer.wei@omsf.io',
