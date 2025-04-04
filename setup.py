85f16d25cb0f6d49ba1f74de96516003b350dc67
Stephen Malina <stephen.malina@dynotx.com>
2025-04-04 13:59:03 -0400

    Upgrade to v2.2.4+dyno

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ else:
 
 setup(
     name='openfold',
-    version='2.2.3+dyno',
+    version='2.2.4+dyno',
     description='A PyTorch reimplementation of DeepMind\'s AlphaFold 2',
     author='OpenFold Team',
     author_email='jennifer.wei@omsf.io',
